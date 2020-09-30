d25121bcb01cbee486de751d388dac8af688607f
tanner.dailey <daileytanner@gmail.com>
2020-09-30 15:35:54 -0600

    finished project

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 require('dotenv').config()
 const express = require('express');
 const massive = require('massive');
+const products_controller = require('./products_controller');
 
 const app = express();
 
-const {SERVER_PORT} = process.env;
-
+const {SERVER_PORT, CONNECTION_STRING} = process.env;
 app.use(express.json());
+
+massive({
+connectionString: CONNECTION_STRING,
+ssl: {rejectUnauthorized: false}
+}).then(dbInstance => {
+    app.set('db', dbInstance);
+}).catch(err => console.log(err));
+
+
+app.get('/api/products', products_controller.getAll);
+app.get('/api/products/:id', products_controller.getOne);
+app.put('/api/products/:id', products_controller.update);
+app.post('/api/products', products_controller.create);
+app.delete('/api/products/:id', products_controller.delete);
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server listening on port ${SERVER_PORT}.`);
